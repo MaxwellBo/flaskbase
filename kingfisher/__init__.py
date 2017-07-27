57e553a511033bbde57034ea05a950bb19a6daf5
Max Bo <max@maxbo.me>
2017-07-27 15:45:25 +1000

    Attempt to support Heroku deploys

--- kingfisher/__init__.py
+++ kingfisher/__init__.py
@@ -1,10 +1,13 @@
+import os
 from .base import app
 from . import routes
 from . import models as m
 from waitress import serve
 
 def main(argv):
-   app.config['SQLALCHEMY_DATABASE_URI'] = None
+   app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DATABASE_URL', 'postgresql://postgres:postgres@localhost/kingfisher')
+   app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
    m.db.init_app(app)
    serve(app) 
 
--- kingfisher/routes.py
+++ kingfisher/routes.py
@@ -1,2 +1,6 @@
 from .base import app
+from flask import jsonify
 
+@app.route('/ping')
+def summary():
+    return jsonify("pong")
