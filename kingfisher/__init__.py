fd44775bde0e2a32342443188a90246139c0d779
Max Bo <max@maxbo.me>
2017-07-28 15:34:07 +1000

    Add Flask Migration support

--- kingfisher/__init__.py
+++ kingfisher/__init__.py
@@ -1,13 +1,16 @@
 import os
 import waitress
+
 from .base import app
 from . import routes
-from . import models as m
+from . import models
 
 def main(argv):
    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     'DATABASE_URL', 'postgresql://postgres:postgres@localhost/kingfisher')
    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-   m.db.init_app(app)
+
+   models.db.init_app(app)
+
    waitress.serve(app)
 
